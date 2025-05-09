ab56d82d8116ffcf7ee24dc669945874d361eda8
Medeirosz <rafaelnp06@gmail.com>
2025-05-08 21:31:45 -0300

    functional test api

--- back/main.py
+++ back/main.py
@@ -1,6 +1,15 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500"],  # só o front pode chamar, Só aceita requisições vindas do front local
+    allow_credentials=True, #Permite cookies/sessões se usar login futuramente
+    allow_methods=["*"], #Permite GET, POST, PUT, etc
+    allow_headers=["*"], #Permite qualquer header necessário, tipo Content-Type: application/json
+)
 
 @app.get("/")
 def home():
