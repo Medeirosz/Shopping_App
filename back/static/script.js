933fc33efb687cf160827b02e3af798e7393b6b5
Medeirosz <rafaelnp06@gmail.com>
2025-05-09 12:06:56 -0300

    minor changes

--- back/static/script.js
+++ back/static/script.js
@@ -14,4 +14,23 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     });
   });
+
+  VANTA.NET({
+    el: "#vanta-bg",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0xaaaaaa,            
+    backgroundColor: 0x0a0a0a,  
+    points: 8.0,
+    maxDistance: 1.0,         
+    spacing: 20.0
+  })
+  
+  
+  
   
