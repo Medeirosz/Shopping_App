281e184add1ccaaedcc7c763e9f144b6ede9f281
Medeirosz <rafaelnp06@gmail.com>
2025-05-09 10:55:33 -0300

    api changes

--- back/main.py
+++ back/main.py
@@ -1,16 +1,48 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
 
 app = FastAPI()
 
+# CORS (pode ser removido se front estiver 100% dentro do back)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],  # só o front pode chamar, Só aceita requisições vindas do front local
-    allow_credentials=True, #Permite cookies/sessões se usar login futuramente
-    allow_methods=["*"], #Permite GET, POST, PUT, etc
-    allow_headers=["*"], #Permite qualquer header necessário, tipo Content-Type: application/json
+    allow_origins=["http://localhost:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.get("/")
-def home():
-    return {"mensagem": "API FastAPI no ar!"}
+# Arquivos estáticos (css, js, imagens)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates HTML
+templates = Jinja2Templates(directory="templates")
+
+# Página inicial (renderiza index.html)
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+
+    produtos = [
+        {"nome": "Teclado", "preco": 299.99},
+        {"nome": "Mouse", "preco": 199.999}
+    ]
+
+    return templates.TemplateResponse("index.html", {
+    "request": request,
+    "produtos": produtos
+})
+
+
+
+# @app.get("/produtos", response_class=HTMLResponse)
+# def produtos(request: Request):
+
+#     produtos = [
+#         {"nome": "Camiseta", "preco": 89.90},
+#         {"nome": "Tênis", "preco": 219.99},
+#     ]
+
+#     return produtos
